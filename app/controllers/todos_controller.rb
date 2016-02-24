015b4187d5565eafcb9baf13cb3695914bcc7282
Tyler <tfisher322@gmail.com>
2016-02-24 16:44:45 -0600

    Tasks in the to do list are now able to be clicked on to view their description. They may also be edited and deleted. We're done.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,13 +17,29 @@ MyApp.get "/todos_list" do
   erb :"todos/todo"
 end
 
+MyApp.get "/edit_todo_form/:todo_id" do
+  @todo = Todo.find_by_id(params[:todo_id])
+  erb :"todos/edit_todo_form"
+end
 
+MyApp.post "/edit_todo/:todo_id" do
+  @todo = Todo.find_by_id(params[:todo_id])
+  @todo.title = params["title"]
+  @todo.description = params["description"]
+  @todo.save
+  erb :"/todos/updated"
+end
 
+MyApp.post "/delete_todo/:todo_id" do
+  @todo = Todo.find_by_id(params[:todo_id])
+  @todo.delete
+  erb :"/todos/deleted"
+end
 
-
-
-
-
+MyApp.get "/todo_page/:todo_id" do 
+  @todo = Todo.find_by_id(params[:todo_id])
+  erb :"/todos/todo_page"
+end
 
 
 MyApp.get "/todo" do
