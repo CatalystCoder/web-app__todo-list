ae5c6b4d64d62b9df3815c257138cb2571b25f53
Tyler <tfisher322@gmail.com>
2016-02-24 15:54:21 -0600

    Got our To do list page set up with edit and delete buttons. All that's left is to make those functional.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,4 @@
 # This controller is for all the CRUD operations related to a Todo.
-MyApp.get "/todo" do
-  @user = User.find_by_id(session["user_id"])
-    if @user != nil
-      erb :"/todos/todo"
-    else
-      erb :"/logins/login"
-    end 
-end
 
 MyApp.get "/new_todo_form" do
   erb :"/todos/new_todo_form"
@@ -18,4 +10,27 @@ MyApp.post "/new_todo" do
   @todo.description = params["description"]
   @todo.save
   erb :"/todos/create_success"
+end
+
+MyApp.get "/todos_list" do
+  @todo = Todo.all
+  erb :"todos/todo"
+end
+
+
+
+
+
+
+
+
+
+
+MyApp.get "/todo" do
+  @user = User.find_by_id(session["user_id"])
+    if @user != nil
+      erb :"/todos/todo"
+    else
+      erb :"/logins/login"
+    end 
 end
