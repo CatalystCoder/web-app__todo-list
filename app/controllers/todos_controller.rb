a5efe6c3ac531eddec2fae5c25ec5ca52f00cc9f
Tyler <tfisher322@gmail.com>
2016-02-24 15:13:41 -0600

    Button for adding a task and its description are in place. Going to add the ability to see items added in the to do list.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,21 @@
 # This controller is for all the CRUD operations related to a Todo.
 MyApp.get "/todo" do
-  @user = User.find_by_id(session["user_id"]) 
-
+  @user = User.find_by_id(session["user_id"])
     if @user != nil
-
-        erb :"todos/todo"
+      erb :"/todos/todo"
     else
-        erb :"logins/login"
+      erb :"/logins/login"
     end 
+end
+
+MyApp.get "/new_todo_form" do
+  erb :"/todos/new_todo_form"
+end
+
+MyApp.post "/new_todo" do
+  @todo = Todo.new
+  @todo.title = params["title"]
+  @todo.description = params["description"]
+  @todo.save
+  erb :"/todos/create_success"
 end
